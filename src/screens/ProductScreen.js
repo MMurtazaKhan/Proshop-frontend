946f362fd255d09ac57d106ecfa1e2e51178129b
MMurtazaKhan <murtaza10dec@gmail.com>
2023-05-06 11:20:43 +0500

    div removed

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -76,9 +76,6 @@ function ProductScreen() {
                 <Col md={6}>
                     <Image src={`https://murtaza10dec.pythonanywhere.com/${product.image}`} alt={product.name} fluid/>
                 </Col>
-                <div>
-                            <h3>https://murtaza10dec.pythonanywhere.com/${product.image}</h3>
-                        </div>
     
                 <Col md={3} >
                     <ListGroup variant='flush' >
